32041100d98ff316472e0ab9df6952af68d9c30e
popohl <pohl@student.42.fr>
2020-12-11 13:16:51 +0100

    Finished philo_one

--- philo_one/incs/philosophers.h
+++ philo_one/incs/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: paulohl <pohl@student.42.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/04 09:01:55 by paulohl           #+#    #+#             */
-/*   Updated: 2020/12/07 13:09:14 by paulohl          ###   ########.fr       */
+/*   Updated: 2020/12/11 13:06:43 by paulohl          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,21 @@
 # define PHILOSOPHERS_H
 
 # include <sys/time.h>
+# include <pthread.h>
 
 # define ACT_EAT	1
 # define ACT_SLEEP	2
 # define ACT_THINK	3
 # define ACT_DIE	4
 # define ACT_DONE	5
+# define CHECK_FREQ 5
 
 typedef int	t_msec;
-
+typedef struct timeval	t_tv;
 typedef struct		s_philosopher
 {
-	int				philosopher_count;
-	int				fork_count;
+	unsigned int	philosopher_count;
+	int				*is_eating;
 	t_msec			time_to_die;
 	t_msec			time_to_eat;
 	t_msec			time_to_sleep;
@@ -35,5 +37,10 @@ typedef struct		s_philosopher
 	pthread_mutex_t	mutex_lock;
 	struct timeval	time_zero;
 }					t_philosopher;
+
+int					make_action(int action, int id, t_philosopher *philosopher);
+int					get_timestamp(struct timeval time_zero);
+int					can_eat(t_philosopher *philosopher, unsigned int id);
+int					is_alive(t_tv time_of_death);
 
 #endif
--- philo_one/settings.mk
+++ philo_one/settings.mk
@@ -2,10 +2,10 @@ ifndef SETTINGS_MK
 	SETTINGS_MK = 1
 
 NAME	= philo_one
-SRCS	= $Smain.c
+SRCS	= $Smain.c $Smake_action.c $Sphilo_checks.c
 
 CC		= gcc
-CFLAGS	+= -Wall -Wextra #-Werror
+CFLAGS	+= -Wall -Wextra -Werror
 LDFLAGS	+= 
 
 endif
