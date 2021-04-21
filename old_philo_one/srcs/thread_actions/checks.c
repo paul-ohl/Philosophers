c8851e5350ab9b7124f86c7eecb7c77e282f91cb
pohl <pohl@student.42.fr>
2021-04-21 19:57:31 +0200

    renamed philo_one and philo_two to old_philo_one and two

---
+++ old_philo_one/srcs/thread_actions/checks.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checks.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: paulohl <pohl@student.42.fr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/11 13:04:02 by paulohl           #+#    #+#             */
+/*   Updated: 2021/04/19 09:24:29 by ft               ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo_one.h"
+
+int	get_time_to_wait(t_msec time_to_check, struct timeval time_of_death)
+{
+	t_msec			tmp;
+	struct timeval	current_time;
+	struct timeval	death_svg;
+
+	death_svg = time_of_death;
+	tmp = time_to_check % 1000;
+	if (time_of_death.tv_usec - (tmp * 1000) < 0)
+	{
+		time_of_death.tv_sec--;
+		time_of_death.tv_usec = time_of_death.tv_usec - (tmp * 1000) + 1000000;
+	}
+	else
+		time_of_death.tv_usec -= tmp * 1000;
+	time_of_death.tv_sec -= time_to_check / 1000;
+	if (is_alive(time_of_death))
+		return (time_to_check * 1000);
+	gettimeofday(&current_time, NULL);
+	tmp = (death_svg.tv_sec - current_time.tv_sec) * 1000;
+	tmp += (death_svg.tv_usec - current_time.tv_usec) / 1000;
+	if (tmp < 0)
+		tmp = 0;
+	return (tmp * 1000);
+}
---
+++ old_philo_one/srcs/utils/free.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ft <marvin@42.fr>                          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/18 19:17:50 by ft                #+#    #+#             */
+/*   Updated: 2021/04/21 19:48:33 by ft               ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo_one.h"
+#include <stdlib.h>
+
+bool	free_config(t_config *config, pthread_t *threads)
+{
+	unsigned int	i;
+
+	if (config)
+	{
+		if (config->forks)
+		{
+			i = 0;
+			while (i < config->philosopher_count)
+				pthread_mutex_destroy(&(config->forks[i++]));
+			free(config->forks);
+		}
+		if (config->time_of_death)
+			free(config->time_of_death);
+		free(config);
+	}
+	if (threads)
+		free(threads);
+	return (false);
+}
